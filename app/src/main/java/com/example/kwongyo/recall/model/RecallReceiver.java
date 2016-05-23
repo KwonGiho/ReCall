9504c87dbf8086c21556928968345eaab6219721
kwongiho <shng10503@naver.com>
2016-05-23 16:37:00 +0900

    login complete

---
+++ app/src/main/java/com/example/kwongyo/recall/model/RecallReceiver.java
@@ -0,0 +1,15 @@
+package com.example.kwongyo.recall.model;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by adminstrator on 2016-05-23.
+ */
+public class RecallReceiver  extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
