1ec8a95fe8f9cab9a54382b3466cb53ead13a81a
kwongiho <shng10503@naver.com>
2016-05-23 21:15:29 +0900

    try first time location and fail

--- app/src/main/java/com/example/kwongyo/recall/core/DataHelperCenter.java
+++ app/src/main/java/com/example/kwongyo/recall/core/DataHelperCenter.java
@@ -1,8 +1,49 @@
 package com.example.kwongyo.recall.core;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by kwongyo on 2016-05-23.
  */
-public class DataHelperCenter {
+public class DataHelperCenter extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="RECALL.db";
+    private static final String MARKER_TABLE ="marker";
+    private static final int DATABASE_VERSION=1;
 
+    private static final String CREATE_MARKER_TABLE=
+            "CREATE TABLE "+MARKER_TABLE+
+                    " (m_id integer autoincrement primary key," +
+                    "address text not null," +
+                    "lat double not null" +
+                    "lng double not null";
+
+    private Context context;
+    private DataHelperCenter(Context context){
+       super(context,DATABASE_NAME,null,DATABASE_VERSION);
+        this.context=context;
+    }
+
+    private static DataHelperCenter dataHelperCenter;
+    public static DataHelperCenter getInstance(Context context) {
+        if(dataHelperCenter == null)
+            dataHelperCenter = new DataHelperCenter(context);
+        return dataHelperCenter;
+    }
+    public static DataHelperCenter getInstance() {
+        return dataHelperCenter;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_MARKER_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        /*새로운 버전이 아닐경우.*/
+        if(oldVersion > newVersion)
+            return ;
+        db.execSQL("DROP TABLE IF EXISTS"+MARKER_TABLE);
+        onCreate(db);
+    }
 }
--- app/src/main/java/com/example/kwongyo/recall/model/RecallReceiver.java
+++ app/src/main/java/com/example/kwongyo/recall/model/RecallReceiver.java
@@ -3,13 +3,83 @@ package com.example.kwongyo.recall.model;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by adminstrator on 2016-05-23.
  */
 public class RecallReceiver  extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Intent locationService = new Intent(context,LocationService.class);
+        context.startService(locationService);
 
     }
+
+
+
 }
+
+
+/*
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        // GPS 프로바이더 사용가능여부
+        isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // 네트워크 프로바이더 사용가능여부
+        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Log.d("Main", "isGPSEnabled="+ isGPSEnabled);
+        Log.d("Main", "isNetworkEnabled="+ isNetworkEnabled);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+
+
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        try {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+
+
+        // 수동으로 위치 구하기
+        String locationProvider = LocationManager.GPS_PROVIDER;
+
+        Location lastKnownLocation=null;
+        try {
+            lastKnownLocation= locationManager.getLastKnownLocation(locationProvider);
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+
+        if (lastKnownLocation != null) {
+            double lng = lastKnownLocation.getLatitude();
+            double lat = lastKnownLocation.getLatitude();
+            Log.d("Main", "longtitude=" + lng + ", latitude=" + lat);
+        }*/
